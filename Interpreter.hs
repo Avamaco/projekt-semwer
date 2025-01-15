55a878ce54e1f0525c99b456dd56f563913a8930
Avamaco <maco_2003@wp.pl>
2025-01-15 02:00:32 +0100

    Added opInt, opBool and opCompare

--- Interpreter.hs
+++ Interpreter.hs
@@ -31,6 +31,30 @@ execProg (Prog stmt) = True
 
 -- Expressions
 
+opInt :: Expr -> Expr -> VEnv -> Store -> (Integer -> Integer -> Integer) -> Either Error Value
+opInt exp0 exp1 rhoV sto op = 
+  let v0 = eE exp0 rhoV sto
+      v1 = eE exp1 rhoV sto
+   in case (v0, v1) of
+        (Right (IntValue i0), Right (IntValue i1)) -> Right (IntValue (op i0 i1))
+        _ -> Left Error
+
+opBool :: Expr -> Expr -> VEnv -> Store -> (Bool -> Bool -> Bool) -> Either Error Value
+opBool exp0 exp1 rhoV sto op = 
+  let v0 = eE exp0 rhoV sto
+      v1 = eE exp1 rhoV sto
+   in case (v0, v1) of
+        (Right (BoolValue b0), Right (BoolValue b1)) -> Right (BoolValue (op b0 b1))
+        _ -> Left Error
+
+opCompare :: Expr -> Expr -> VEnv -> Store -> (Integer -> Integer -> Bool) -> Either Error Value
+opCompare exp0 exp1 rhoV sto op = 
+  let v0 = eE exp0 rhoV sto
+      v1 = eE exp1 rhoV sto
+   in case (v0, v1) of
+        (Right (IntValue i0), Right (IntValue i1)) -> Right (BoolValue (op i0 i1))
+        _ -> Left Error
+
 eE :: Expr -> VEnv -> Store -> Either Error Value
 
 eE (ETrue) rhoV sto = Right (BoolValue True)
@@ -38,23 +62,37 @@ eE (EFalse) rhoV sto = Right (BoolValue False)
 eE (ENum n) rhoV sto = Right (IntValue n)
 eE (EVar v) rhoV sto = Right (IntValue 0) --TODO
 
-eE (EPlus exp0 exp1) rhoV sto = (eE exp0 rhoV sto) + (eE exp1 rhoV sto)
-eE (EMinus exp0 exp1) rhoV sto = (eE exp0 rhoV sto) - (eE exp1 rhoV sto)
-eE (EMul exp0 exp1) rhoV sto = (eE exp0 rhoV sto) * (eE exp1 rhoV sto)
-eE (EDiv exp0 exp1) rhoV sto = (eE exp0 rhoV sto) / (eE exp1 rhoV sto)
-eE (ENeg exp) rhoV sto = - (eE exp rhoV sto)
+eE (EPlus exp0 exp1) rhoV sto = opInt exp0 exp1 rhoV sto (+)
+eE (EMinus exp0 exp1) rhoV sto = opInt exp0 exp1 rhoV sto (-)
+eE (EMul exp0 exp1) rhoV sto = opInt exp0 exp1 rhoV sto (*)
+eE (EDiv exp0 exp1) rhoV sto = opInt exp0 exp1 rhoV sto (div)
+eE (ENeg exp) rhoV sto = 
+  let v = eE exp rhoV sto
+   in case v of
+        Right (IntValue i) -> Right (IntValue (-i))
+        _ -> Left Error
 
-eE (EEq exp0 exp1) rhoV sto = (eE exp0 rhoV sto) == (eE exp1 rhoV sto)
-eE (ELt exp0 exp1) rhoV sto = (eE exp0 rhoV sto) < (eE exp1 rhoV sto)
-eE (EGt exp0 exp1) rhoV sto = (eE exp0 rhoV sto) > (eE exp1 rhoV sto)
-eE (ELeq exp0 exp1) rhoV sto = (eE exp0 rhoV sto) <= (eE exp1 rhoV sto)
-eE (EGeq exp0 exp1) rhoV sto = (eE exp0 rhoV sto) >= (eE exp1 rhoV sto)
-eE (ENeq exp0 exp1) rhoV sto = (eE exp0 rhoV sto) /= (eE exp1 rhoV sto)
+eE (EEq exp0 exp1) rhoV sto = opCompare exp0 exp1 rhoV sto (==)
+eE (ELt exp0 exp1) rhoV sto = opCompare exp0 exp1 rhoV sto (<)
+eE (EGt exp0 exp1) rhoV sto = opCompare exp0 exp1 rhoV sto (>)
+eE (ELeq exp0 exp1) rhoV sto = opCompare exp0 exp1 rhoV sto (<=)
+eE (EGeq exp0 exp1) rhoV sto = opCompare exp0 exp1 rhoV sto (>=)
+eE (ENeq exp0 exp1) rhoV sto = opCompare exp0 exp1 rhoV sto (/=)
 
-eE (EAnd exp0 exp1) rhoV sto = (eE exp0 rhoV sto) && (eE exp1 rhoV sto)
-eE (EOr exp0 exp1) rhoV sto = (eE exp0 rhoV sto) || (eE exp1 rhoV sto)
-eE (ENot exp) rhoV sto = not (eE exp rhoV sto)
+eE (EAnd exp0 exp1) rhoV sto = opBool exp0 exp1 rhoV sto (&&)
+eE (EOr exp0 exp1) rhoV sto = opBool exp0 exp1 rhoV sto (||)
+eE (ENot exp) rhoV sto =
+  let v = eE exp rhoV sto
+   in case v of
+        Right (BoolValue b) -> Right (BoolValue (not b))
+        _ -> Left Error
 
-eE (ETern exp0 exp1 exp2) rhoV sto = if (eE exp0 rhoV sto) then (eE exp1 rhoV sto) else (eE exp2 rhoV sto)
+eE (ETern exp0 exp1 exp2) rhoV sto =
+  let v0 = eE exp0 rhoV sto
+      v1 = eE exp1 rhoV sto
+      v2 = eE exp2 rhoV sto
+   in case (v0, v1, v2) of
+        (Right (BoolValue b), Right (IntValue i1), Right (IntValue i2)) -> Right (IntValue (if b then i1 else i2))
+        _ -> Left Error
 
 eE (ECheck exp i) rhoV sto = Right (IntValue 0) --TODO
